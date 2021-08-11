1d5863f8ce6d63f8d19457269845af2539e4b176
Irupc <68991705+Irupc@users.noreply.github.com>
2021-08-11 22:58:54 +0530

    Update __main__.py

--- JESongBot/__main__.py
+++ JESongBot/__main__.py
@@ -36,7 +36,7 @@ async def start(client, message):
         )
     else:
         btn = None
-    await message.reply(pm_start_text.format(name, user_id), reply_markup=btn)
+    await message.reply(pm_start_text.format(name, user_id), disable_web_page_preview=True, reply_markup=btn)
 
 
 app.start()
